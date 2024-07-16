910604c0818241c35c6595fb4d5717c73765b608
ZoomL-Olymp <nnahalenok@gmail.com>
2024-07-16 22:43:09 +0300

    Optimization for mobile

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-// script.js
-
+// Анимация появления секций при прокрутке
 window.addEventListener('scroll', () => {
-    const sections = document.querySelectorAll('.hero, .services, .approach, .documentation, .contacts');
+  const sections = document.querySelectorAll('.hero, .services, .approach, .documentation, .contacts');
 
-    sections.forEach(section => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const windowHeight = window.innerHeight;
+  sections.forEach(section => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
 
-        if (sectionTop < windowHeight * 0.8) {
-            section.classList.add('appear');
-        }
-    });
+      if (sectionTop < windowHeight * 0.8) {
+          section.classList.add('appear');
+      }
+  });
 });
 
-
+// Раскрывающийся блок в секции "Документация"
 const toggleInfoButtons = document.querySelectorAll(".toggle-info");
 
 toggleInfoButtons.forEach(button => {
   button.addEventListener("click", (event) => {
-    event.preventDefault(); 
-    const additionalInfo = button.previousElementSibling; 
-    additionalInfo.classList.toggle("show"); 
+      event.preventDefault();
+      const additionalInfo = button.previousElementSibling;
+      additionalInfo.classList.toggle("show");
 
-    button.textContent = additionalInfo.classList.contains("show")
-      ? "Скрыть"
-      : "Подробнее";
+      button.textContent = additionalInfo.classList.contains("show")
+          ? "Скрыть"
+          : "Подробнее";
   });
 });
